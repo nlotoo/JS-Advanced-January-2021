21e52421bfc0b928b069424c95c26609752983c6
Yordan Nikolov <65885882+nlotoo@users.noreply.github.com>
2021-01-17 13:21:59 +0000

    Add files via upload

---
+++ GSD.js
@@ -0,0 +1,15 @@
+function GSD(x, y) {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  
+  console.log(GSD(15, 5));
+  console.log(GSD(2154, 458));
