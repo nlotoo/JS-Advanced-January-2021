8030a87e3f6628fe3cbcb997e5c09fe3f5cf5456
Yordan Nikolov <65885882+nlotoo@users.noreply.github.com>
2021-01-17 13:15:10 +0000

    Add files via upload

---
+++ sameNum.js
@@ -0,0 +1,19 @@
+function solve(number) {
+
+    const string = number.toString();
+    let isSame = true;
+    let sum = 0;
+
+
+    for (let i = 0; i < string.length; i++) {
+        let current = Number(string[i]);
+        if (string[i] !== string[i + 1]) {
+            isSame = false;
+        }
+        sum += current;
+    }
+    console.log(`${isSame}\n${sum}`)
+
+}
+//solve(1234)
+solve(2222222)
