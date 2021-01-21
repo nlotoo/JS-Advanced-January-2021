b9a31f61651b0d70237805ebb891e8f615251052
Yordan Nikolov <65885882+nlotoo@users.noreply.github.com>
2021-01-21 17:12:43 +0000

    Update subtract.js

--- EXERCISE DOM INTRODUCTION/01. Subtraction/DOM-Introduction-Exercise/01. Subtraction/subtract.js
+++ EXERCISE DOM INTRODUCTION/01. Subtraction/DOM-Introduction-Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    console.log('TODO:...');
+   
+    const firstNum= Number(document.getElementById('firstNumber').value);
+    const secondNum= Number(document.getElementById('secondNumber').value);
+
+const resulta=firstNum-secondNum
+
+    const result=document.getElementById('result').textContent =resulta
+ 
 }
