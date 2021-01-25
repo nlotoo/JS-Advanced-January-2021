c65dc7f658eb53bb98d29cc32d4d7602c7999573
Yordan Nikolov <nlotoo93@gmail.com>
2021-01-25 20:49:30 +0000

    Create subtract.js

---
+++ ЕXERCISE DOM/01. Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+    const firstNum = Number(document.getElementById('firstNumber').value);
+    const secondNum = Number(document.getElementById('secondNumber').value);
+
+    const resulta = firstNum - secondNum
+
+    const result = document.getElementById('result').textContent = resulta
+}
